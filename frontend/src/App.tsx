6f33b9c7c2ed23113e26cefbf35f686c9564376a
Tenderly__ <olevnao31@gmail.com>
2026-06-02 17:31:12 +0300

    chore(frontend): initialize shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="flex min-h-svh items-center justify-center bg-background">
+        <div className="space-y-4 text-center">
+          <h1 className="text-3xl font-bold">Translator App</h1>
+          <Button>shadcn/ui</Button>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
