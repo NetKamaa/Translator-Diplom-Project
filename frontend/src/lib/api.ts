682a8b6128314ea389135f0d6f306f6df57e4c6e
Tenderly__ <olevnao31@gmail.com>
2026-06-04 21:00:44 +0300

    feat(frontend): add api.ts (axios)

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
